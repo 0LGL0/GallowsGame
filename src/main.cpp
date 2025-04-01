f8952aae45ef18788d097c446b6dd2a92044d765
0LGL0 <artemkohan36@gmail.com>
2025-04-01 19:11:49 +0300

    Now you can enter only letters and only one letter

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,34 @@
+#include <cstdlib>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <limits>
 #include <string>
 #include <vector>
-#include <cstdlib>
+#if defined(__linux__)
+#include <cctype>
+#include <iomanip>
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#elif defined(__WIN32) || defined(WIN64)
+
+#endif
+
+// void getChar(char &letter) {
+// #if defined(__linux__)
+//   struct termios tNew, tOld;
+//   tcgetattr(STDIN_FILENO, &tOld);
+//   tNew = tOld;
+//   tNew.c_lflag &= ~(ICANON);
+//   tcsetattr(STDIN_FILENO, TCSANOW, &tNew);
+//   letter = getchar();
+//
+//   tcsetattr(STDIN_FILENO, TCSANOW, &tOld);
+// #elif defined(__WIN64) || defined(__WIN32)
+//   letter = _getch();
+// #endif
+// }
 
 const std::string parseFile(const std::string path,
                             const unsigned short lineNum) {
@@ -23,7 +48,6 @@ const std::string parseFile(const std::string path,
       resWord = buffer;
       break;
     }
-    // std::cout << buffer << std::endl;
   }
 
   file.close();
@@ -32,7 +56,8 @@ const std::string parseFile(const std::string path,
 }
 
 int main() {
-  std::filesystem::path pathToWordsFile = std::filesystem::current_path();
+  std::filesystem::path pathToWordsFile =
+      std::filesystem::current_path().remove_filename();
   pathToWordsFile += "/res/words.txt";
   // std::cout << pathToWordsFile.string() << std::endl;
 
@@ -50,16 +75,15 @@ int main() {
   std::cout << famousWord << std::endl;
 
   unsigned attemptsCount = 0;
-  std::string theGallows =
-      "########## \n"
-      " #/     |  \n"
-      " #         \n"
-      " #         \n"
-      " #         \n"
-      " #         \n"
-      " #           ";
+  std::string theGallows = "########## \n"
+                           " #/     |  \n"
+                           " #         \n"
+                           " #         \n"
+                           " #         \n"
+                           " #         \n"
+                           " #           ";
   while (true) {
-#if defined(__linux__) || defined(__unix) || defined(__unjx__)
+#if defined(__linux__)
     std::system("clear");
 #elif defined(_WIN64) || defined(_WIN32)
     std::system("cls");
@@ -68,15 +92,26 @@ int main() {
     char userLetter;
     bool isLetterCorrect = false;
 
-    switch(attemptsCount){
-      case 1: theGallows[32] = 'O'; break;
-      case 2: theGallows[44] = '&'; break;
-      case 3: theGallows[43] = '/'; break;
-      case 4: theGallows[45] = '\\'; break;
-      case 5: theGallows[55] = '/'; break;
-      case 6: theGallows[57] = '\\'; 
+    switch (attemptsCount) {
+    case 1:
+      theGallows[32] = 'O';
+      break;
+    case 2:
+      theGallows[44] = '&';
+      break;
+    case 3:
+      theGallows[43] = '/';
+      break;
+    case 4:
+      theGallows[45] = '\\';
+      break;
+    case 5:
+      theGallows[55] = '/';
+      break;
+    case 6:
+      theGallows[57] = '\\';
     }
-    std::cout << theGallows << std::endl << std::endl; 
+    std::cout << theGallows << std::endl << std::endl;
 
     if (attemptsCount == 6) {
       std::cout << "Sorry, you lose ^)" << std::endl;
@@ -86,23 +121,30 @@ int main() {
                 << " attempts";
       break;
     }
-    
-    std::cout << "Count of attempts: \033[1;34m" << attemptsCount << "\033[0m" << std::endl;
+
+    std::cout << "Count of attempts: \033[1;34m" << attemptsCount << "\033[0m"
+              << std::endl;
     std::cout << "Word you need to guess: " << famousWord << std::endl;
-    std::cout << "\033[1;31mYou can press 'Ctrl' + 'C' to quit\033[0m" << std::endl;
-    std::cin >> userLetter;
+    std::cout << "\033[1;31mYou can press 'Ctrl' + 'C' to quit\033[0m"
+              << std::endl;
+    do {
+      // getChar(userLetter);
+      std::cin >> userLetter;
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    } while (!std::isalpha(userLetter));
 
     std::vector<unsigned> indexes;
     for (unsigned i = 0; i < guessedWord.size(); i++) {
-      if (guessedWord[i] == userLetter || guessedWord[i] == (userLetter + 32) || guessedWord[i] == (userLetter - 32)) {
-        if (guessedWord[i] != famousWord[i]){
+      if (guessedWord[i] == userLetter || guessedWord[i] == (userLetter + 32) ||
+          guessedWord[i] == (userLetter - 32)) {
+        if (guessedWord[i] != famousWord[i]) {
           famousWord[i] = guessedWord[i];
           isLetterCorrect = true;
         }
       }
     }
 
-    if(!isLetterCorrect)
+    if (!isLetterCorrect)
       attemptsCount++;
   }
 
